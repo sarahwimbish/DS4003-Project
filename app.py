6eadeca78365526cb110e128fc18ed311e043144
Sarah Wimbish <smwimbish@verizon.net>
2024-04-03 23:23:51 -0400

    updated dashboard and app.py

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ import plotly as py
 import plotly.graph_objs as go
 import matplotlib.pyplot as plt
 import numpy as np
+import mpld3
 import plotly.tools as tls
 from io import BytesIO
 import base64
+import plotly.express as px 
 
 
 
@@ -17,6 +19,8 @@ import base64
 stylesheet = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, external_stylesheets=stylesheet)
 server = app.server
+app.css.append_css({"external_url": "/assets/main.css"})
+app.server.static_folder = "assets"
 
 data = pd.read_csv('data/data.csv')
 data['date'] = pd.to_datetime(data['date'])
@@ -39,9 +43,11 @@ data['date'] = pd.to_datetime(data['date'])
 colors = ['#4487b9', '#e4a23a', '#a18b31', '#cae2e9', '#93a5d0', '#eed9cb']
 color = '#4487b9'
 stroke = 2
+curve = 10
+border = ' #FFFFFF'
 
 left1 = [
-    html.H2("My Listening Habits", style={'textAlign': 'center'}),
+    html.H2(["My Listening", html.Br(), "Habits"], style={'textAlign': 'center'}),
     html.P(id="streaming_minutes", style={'color':'white', 'textAlign': 'center', 'font-weight': 'bold', 'font-size':'60px',
                                            "text-shadow":f' -1px -{stroke}px 0 {color}, 1px -{stroke}px 0 {color}, \
                                             -1px {stroke}px 0 {color}, 1px {stroke}px 0 {color}, \
@@ -70,11 +76,21 @@ left1 = [
     html.Center(id = 'date_annotation')
 ]
 
+top_artists = [
+    html.Div(html.H4(["Top", html.Br(), 'Artists']), className = 'three columns'), 
+    html.Div(html.P("widgets"), className = 'three columns'), 
+    html.Div(html.P("widgets"), className = 'three columns')
+]
 ############################################################################################################################################
 #######################################################            IMAGE EXPORT           ##################################################
 ############################################################################################################################################
 
 def fig_to_uri(in_fig, close_all=True, **save_args):
+    """
+    Save a figure as a URI
+    :param in_fig:
+    :return:
+    """
     out_img = BytesIO()
     in_fig.savefig(out_img, format='png', **save_args)
     if close_all:
@@ -88,45 +104,81 @@ def fig_to_uri(in_fig, close_all=True, **save_args):
 ###########################################################            APP           #######################################################
 ############################################################################################################################################
 
+border = 'white'
+
 app.layout = html.Div( # entire page
     [
         html.Div( # Top of Page 
             [
                 html.H1("Sarah's Spotify Dashboard"), # title 
                 html.H3("blah blah blah "), # description
-            ],style={"border":"2px solid pink", 'margin':'5px', 'textAlign': 'center'}),
+            ],style={"border":"2px {boarder}", 
+                     'textAlign': 'center', 
+                     'background-color':'white', 
+                     'border-radius': f'{curve}px {curve}px {curve}px {curve}px',
+                     'margin':'0px 5px'}),
+
 
         html.Div(# Bottom of Page 
             [ 
-                        html.Div(left1, # left 3 col of left 
-                                className= "three columns", 
-                                style={'margin':'5px', "border":"2px solid pink", 'align-items':'center', 'justify-content':'center'}), 
-                        
-                        html.Div(# right 6 col of left 
-                            [html.Div([
-                                        "WIDGETS",
-                                        dcc.DatePickerRange(
-                                            id='date-slider',
-                                            min_date_allowed= data['date'].min(),
-                                            max_date_allowed= data['date'].max(),
-                                            start_date = data['date'].min(),
-                                            end_date = data['date'].max()),
-                                        html.Button('RESET', id='reset-button', n_clicks=0),
-                                        ], style={'border':'2px solid pink', 'margin':'5px'}),
-                             html.Div([dcc.Graph(id='line-plot')], style={'border':'2px solid pink','margin':'5px'}),
-                             html.Div("MORE WIDGETS", style={'border':'2px solid pink', 'margin':'5px'}), 
-                            
-                            ], className='six columns', style ={'margin':'5px'}),
 
-                        html.Div(
-                                [ html.Img(id = 'plot1', src = '', style={'width': '400px', 'height': '400px'}),
-                                            dcc.RadioItems(options = ["AM", "PM"],
-                                                        value = "AM",
-                                                        inline = True,
-                                                        id='AMPM-radio',
-                                                        style={'textAlign': 'center', 'margin':'5px'})
-                                ], className='three columns', style={'margin':'5px'})
-    ], style={"border":"2px solid pink"}, className = 'row') ])
+                html.Div(left1, # left 3 col of left 
+                        className= "container1", 
+                        style={"border":"2px {boarder}", 
+                                'align-items':'center', 
+                                'justify-content':'center', 
+                                'background-color':'white', 
+                                'border-radius': f'{curve}px {curve}px {curve}px {curve}px',
+                                'margin':'0px 5px'}), 
+                
+
+
+                html.Div(# right 6 col of left 
+                    [html.Div([
+                                dcc.DatePickerRange(
+                                    id='date-slider',
+                                    min_date_allowed= data['date'].min(),
+                                    max_date_allowed= data['date'].max(),
+                                    start_date = data['date'].min(),
+                                    end_date = data['date'].max()),
+                                html.Button('RESET DATE', id='reset-button', n_clicks=0, style={'background-color': 'white', "margin-left": "15px"}),
+                                ], style={'border':'2px {border}',
+                                            'border-radius': f'{curve}px {curve}px {curve}px {curve}px',
+                                            'background-color': 'white',
+                                            'padding':'10px',
+                                            'margin':'0px 0px 5px 0px'}),
+                        html.Div([dcc.Graph(id='line-plot')], style={'border':'2px {border}',
+                                                                    'border-radius':f'{curve}px {curve}px {curve}px {curve}px',
+                                                                    'background-color': 'white',
+                                                                    'padding':'5px',
+                                                                    'margin':'5px 0px 0px 0px'}),
+                        html.Div(top_artists, style={'background-color':'white', 
+                                                    'border-radius': f'{curve}px {curve}px {curve}px {curve}px',
+                                                    'margin':'5px 0px', 'padding':'5px'}), 
+                    
+                    ], className='container2', style ={'margin':'0px 5px'}),
+
+
+
+                    html.Div( ## right panel 
+                        [html.Div([html.Img(id = 'plot1', src = '', style={'width': '320px',  ### want 380px but moves out of the div 
+                                                                    'height': '320px',
+                                                                    'padding':'5px'}),
+                        dcc.RadioItems(options = ["AM", "PM"],
+                                    value = "AM",
+                                    inline = True,
+                                    id='AMPM-radio',
+                                    style={'textAlign': 'center'})
+                        ], style = {'background-color':'white', 
+                                    'border-radius': f'{curve}px {curve}px {curve}px {curve}px',
+                                    'margin':'0px 5px'}), 
+                        html.Div(dcc.Graph(id='bar-plot'), style ={'background-color':'white', 
+                                                                     'border-radius': f'{curve}px {curve}px {curve}px {curve}px',
+                                                                     'margin':'5px 5px', 
+                                                                     'padding':'5px' })
+                        ], className = 'container3', style={})
+    ], style={"border":"2px {border}", 'margin-top': '0px'}) ], className='row', style={'backgroundColor':'pink'})
+
 
 if __name__ == '__main__':
     app.run_server(jupyter_mode='tab', debug=True)
@@ -174,9 +226,9 @@ def get_different_songs(start, end):
         Input('date-slider', 'end_date')
 )
 def get_date_annotations(start, end):
-    return f'Between \n{start} and {end}'
+    return html.P([f'Between {start.strip("T00:00:00")}', html.Br(), f'{end.strip("T00:00:00")}'])
 
-@app.callback(
+@callback(
     Output('date-slider', 'start_date'),
     Output('date-slider', 'end_date'),
     Input('reset-button', 'n_clicks')
@@ -211,7 +263,7 @@ def create_plot1(hours_selected, start, end):
 
     hours_to_use = hoursam
     AMPM = "AM"
-    color1 = '##e5a13a'
+    color1 = '#fca828'
 
     if(hours_selected=="AM"):
         hours_to_use = hoursam
@@ -301,7 +353,6 @@ def create_plot1(hours_selected, start, end):
     out_url = fig_to_uri(fig)
     return out_url
 
-
 @callback(
     Output('line-plot', 'figure'),
     Input('date-slider', 'start_date'),
@@ -348,9 +399,35 @@ def create_line_plot(start, end):
     ))
 
     fig.update_layout(template='plotly_white',
-                    legend=dict(yanchor="bottom", xanchor='center', x = .5, y= -.15, orientation = 'h'))
+                    legend=dict(yanchor="bottom", xanchor='center', x = .5, y= -.15, orientation = 'h'),
+                    height = 400)
                     
     fig.update_layout(
         margin=dict(l=50, r=50, b=50, t=0)
     )
     return fig
+
+@callback(
+    Output('bar-plot', 'figure'),
+    Input('date-slider', 'start_date'),
+    Input('date-slider', 'end_date')
+)
+def create_bar_plot(start, end):
+    data_filtered = data[data['date'].between(start, end)]
+    times = data_filtered.groupby(['timeofday'], as_index=False)['minutes'].sum()
+    times['timeofday'] = ['12pm-6pm', "5am-12pm", "6pm-5am"]
+    times['timeofday'] = times['timeofday'].astype('category')
+    times['timeofday'] = times['timeofday'].cat.reorder_categories(["5am-12pm", '12pm-6pm', '6pm-5am'])
+
+    fig = go.Figure()
+    fig = px.bar(times.sort_values('timeofday'), 
+                x='timeofday', 
+                y='minutes',
+                color = 'timeofday',
+                color_discrete_map ={'5am-12pm': '#eba8c6', '6pm-5am': '#4487b9', '12pm-6pm':'#e5a13a'}, 
+                template = 'plotly_white', 
+                labels = {'minutes': 'Total Listening Minutes',
+                        'timeofday':' '}, 
+                height=250)
+    fig.layout.update(showlegend=False) 
+    return fig 
